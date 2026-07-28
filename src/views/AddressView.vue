1023409a0a74363950e9bd013c83a54b591055e5
Eli <imeelinew@gmail.com>
2026-07-28 14:59:22 +0800

    baseline

--- src/views/AddressView.vue
+++ src/views/AddressView.vue
@@ -2,7 +2,9 @@
 import { getAddressList } from '@/api/order';
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { addAddress,getAreaList } from '@/api/address';
+import { addAddress, getAreaList } from '@/api/address';
+import { showSuccessToast, showFailToast } from 'vant';
+import 'vant/es/toast/style';
 const router = useRouter();
 
 //三种模式切换，展示，添加，编辑
@@ -23,10 +25,41 @@ const openAreaCascader = async () => {
     areaOptions.value = res.data.map((item: any) => ({
         text: item.areaName,
         value: item.areaId,
+        children: [], // 有children字段(哪怕是空的)，告诉组件"后面还有下一层，先别结束选择"
     }))
     showAreaCascader.value = true
 }
 const areaText = ref('')
+const provinceId = ref<number>()
+const cityId = ref<number>()
+const areaIdValue = ref<number>()
+const provinceName = ref('')
+const cityName = ref('')
+const areaName = ref('')
+
+//用户选中一层后，问后端要下一层数据，挂到这一层的children上
+const onCascaderChange = async ({ selectedOptions }: any) => {
+    const current = selectedOptions[selectedOptions.length - 1]
+    const res = await getAreaList(current.value)
+    const isLastLevel = selectedOptions.length === 2 // 选完市，取的是区，区是最后一层，不能带children
+    current.children = res.data.map((item: any) =>
+        isLastLevel
+            ? { text: item.areaName, value: item.areaId }
+            : { text: item.areaName, value: item.areaId, children: [] }
+    )
+}
+
+//省市区三层全部选完，触发这个
+const onCascaderFinish = ({ selectedOptions }: any) => {
+    areaText.value = selectedOptions.map((item: any) => item.text).join('')
+    provinceId.value = selectedOptions[0].value
+    provinceName.value = selectedOptions[0].text
+    cityId.value = selectedOptions[1].value
+    cityName.value = selectedOptions[1].text
+    areaIdValue.value = selectedOptions[2].value
+    areaName.value = selectedOptions[2].text
+    showAreaCascader.value = false
+}
 //添加地址
 const handleAddAddress = async () => {
     try {
@@ -34,25 +67,26 @@ const handleAddAddress = async () => {
             receiver: receiver.value,
             mobile: mobile.value,
             addr: addr.value,
-            //test
-            area: 1,
-            cityId: 1,
-            areaId: 1,
-            city: 1,
-            province: 1,
-            provinceId: 1,
-
+            province: provinceName.value,
+            provinceId: provinceId.value,
+            city: cityName.value,
+            cityId: cityId.value,
+            area: areaName.value,
+            areaId: areaIdValue.value,
         })
-        receiver.value = res.data.receiver;
-        mobile.value = res.data.mobile;
-        addr.value = res.data.addr;
+
+        if (res.success) {
+            showSuccessToast('新增地址成功')
+            const listRes = await getAddressList()
+            addressList.value = listRes.data || []
+            mode.value = 'list'
+        } else {
+            showFailToast(res.msg || '新增地址失败')
+        }
     } catch (error) {
         console.log(error, 'error添加地址')
-    } finally {
-        mode.value = 'list';
+        showFailToast('新增地址失败')
     }
-
-
 }
 onMounted(async () => {
     const res = await getAddressList()
@@ -75,7 +109,12 @@ onMounted(async () => {
         </div>
         <div v-else-if="mode === 'add'">
             <van-nav-bar title="添加地址" left-text="返回" left-arrow @click-left="mode = 'list'" />
-            <van-field v-model="receiver" label="地区" placeholder="请选择地区" @click="openAreaCascader" />
+            <van-field v-model="receiver" label="收件人" placeholder="请输入收件人姓名" />
+            <van-field v-model="areaText" label="地区" placeholder="请选择地区" readonly is-link @click="openAreaCascader" />
+            <van-popup v-model:show="showAreaCascader" position="bottom" round>
+                <van-cascader title="请选择地区" :options="areaOptions" @change="onCascaderChange"
+                    @finish="onCascaderFinish" @close="showAreaCascader = false" />
+            </van-popup>
             <van-field v-model="mobile" label="手机号" placeholder="请输入手机号" />
             <van-field v-model="addr" label="详细地址" placeholder="请输入详细地址" />
             <van-button type="danger" block @click="handleAddAddress">保存</van-button>
